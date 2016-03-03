7693578de9643a26676ec44b9474b34d56fe2e56
Marco Schmid <marco.ms.schmid@gmail.com>
2016-03-03 21:37:06 +0100

    basic api

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var port = 8080;
+ 
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json({message: 'hello api'});
+});
+
+app.use('/api', router);
+app.listen(port);
