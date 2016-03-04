1dc6acc5142089865e87137c21671cc9be148544
Marco Schmid <marco.ms.schmid@gmail.com>
2016-03-04 09:11:58 +0100

    added env-port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var path = require("path");
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost');
